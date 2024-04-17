b45e39dce0c2ebc257396bf1cfc7a232be7b6054
Romain JACQUET <rjacquet31@gmail.com>
2024-04-17 17:09:35 +0200

    first version that work with a promise

---
+++ gw_grpc_wait.mjs
@@ -0,0 +1,92 @@
+import { Metadata, credentials} from '@grpc/grpc-js';
+import { GatewayServiceClient } from '@chirpstack/chirpstack-api/api/gateway_grpc_pb.js';
+import gw_pb_pkg from '@chirpstack/chirpstack-api/api/gateway_pb.js';
+
+
+class GatewayManager {
+    GatewayManager(server, api_token){
+        this.server = server;
+        this.api_token = apiToken;
+        this.metadata = new Metadata();
+        this.metadata.set("authorization", "Bearer " + apiToken);
+    }
+
+    async initialize(){
+        const gwDevices = new GatewayServiceClient(
+            server, 
+            credentials.createInsecure());
+        
+        // create the request
+        let lstGWReq = new gw_pb_pkg.ListGatewaysRequest();
+        lstGWReq.setLimit(20);
+        lstGWReq.setOffset(0);
+        
+        
+        // Create the Metadata object.
+        const metadata = new Metadata();
+        metadata.set("authorization", "Bearer " + apiToken);
+        
+        
+        let myPromise = new Promise(function(myResolve, myReject) {
+            gwDevices.list(lstGWReq, metadata, (err, answer) => {
+                if(err){                    
+                    myReject(err)
+                } else {
+                    let gatewayInfo = {};
+                    for(const gwItem of answer.getResultList() ){
+                        console.log("euid:" + gwItem.getGatewayId());
+                        gatewayInfo[gwItem.getGatewayId()] = {
+                            "name": gwItem.getName(),
+                            "desc": gwItem.getDescription(),
+                            "lat": gwItem.getLocation().getLatitude(),
+                            "lon":  gwItem.getLocation().getLongitude()
+                        };
+                    }
+                    console.log("Gateway:" + gatewayInfo);
+                    myResolve(gatewayInfo);
+                }
+            } );            
+          });
+        return myPromise;        
+    }
+
+    queryServer(lstGWReq){
+        let myPromise = new Promise(function(myResolve, myReject) {
+            gwDevices.list(lstGWReq, this.metadata, (err, answer) => {
+                if(err){                    
+                    myReject(err)
+                } else {
+                    let gatewayInfo = {};
+                    for(const gwItem of answer.getResultList() ){
+                        console.log("euid:" + gwItem.getGatewayId());
+                        gatewayInfo[gwItem.getGatewayId()] = {
+                            "name": gwItem.getName(),
+                            "desc": gwItem.getDescription(),
+                            "lat": gwItem.getLocation().getLatitude(),
+                            "lon":  gwItem.getLocation().getLongitude()
+                        };
+                    }
+                    console.log("Gateway:" + gatewayInfo);
+                    myResolve(gatewayInfo);
+                }
+            } );            
+          });
+        return myPromise;        
+    }
+}
+
+// This must point to the ChirpStack API interface.
+const server = "192.168.59.101:31102";
+
+
+
+// The API token (can be obtained through the ChirpStack web-interface).
+const apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6IjhhNjVjYmRhLTgyYzMtNGQ1MS05YzQ2LTBiN2NiMjU2MDRjOCIsInR5cCI6ImtleSJ9.JFal9wQB20pORaO8-ANffM1J8q-hozoj1r6pskohAKo";
+let gwManager = new GatewayManager(server, apiToken);
+let myPromise = gwManager.initialize();
+let gwList = await myPromise.then(
+    function(value) {return value;},
+    function(error) {return {};});
+console.log(JSON.stringify(gwList));
+
+console.log("End.");
